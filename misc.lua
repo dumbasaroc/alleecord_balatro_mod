bf0b3018c5cebdfabbc78cd06e6ba9c3c2a4578b
DumbAsARoc <timbishop514@gmail.com>
2025-04-10 12:35:56 -0400

    indent using spaces LOL

--- misc.lua
+++ misc.lua
@@ -1,9 +1,9 @@
 -- Custom localization colors
 local lc = loc_colour
 function loc_colour(_c, _default)
-	if not G.ARGS.LOC_COLOURS then
-		lc()
-	end
-	G.ARGS.LOC_COLOURS.snaking_straights = HEX("ab749a")
-	return lc(_c, _default)
+    if not G.ARGS.LOC_COLOURS then
+        lc()
+    end
+    G.ARGS.LOC_COLOURS.snaking_straights = HEX("ab749a")
+    return lc(_c, _default)
 end
