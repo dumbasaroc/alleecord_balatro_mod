cfde603f6807bf0510e89a6a38c84d947f1bf16f
DumbAsARoc <timbishop514@gmail.com>
2025-04-13 20:40:58 -0400

    dumbasaroc joker description changed to the random version

--- jokers/dumbasaroc.lua
+++ jokers/dumbasaroc.lua
@@ -18,8 +18,7 @@ SMODS.Joker {
     loc_txt = {
         name = "DumbAsARoc",
         text = {
-            "Each played {C:attention}2{}, {C:attention}4{}, and {C:attention}8{} gives",
-            "{C:chips}+#1#{} Chips and {C:mult}+#2#{} Mult when scored."
+            ""
         },
     },
     discovered = true,
@@ -42,11 +41,33 @@ SMODS.Joker {
     },
 
     loc_vars = function(self, info_queue, card)
-		
+
+        chips_rand = {"1", "2", "4", "8", "16", "32", "64"}
+        mult_rand = {"1", "2", "4", "8", "16"}
+        
         return {
-            vars = {
-                card.ability.extra.chips_per,
-                card.ability.extra.mult_per,
+            main_start = {
+                {n = G.UIT.C, config={align='tm'}, nodes = {
+                    {n=G.UIT.R, config={align="cm"}, nodes = {
+                        {n = G.UIT.T, config={text = "Each scored ", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                        {n = G.UIT.T, config={text = "2", colour=G.C.IMPORTANT, scale=0.32}},
+                        {n = G.UIT.T, config={text = ", ", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                        {n = G.UIT.T, config={text = "4", colour=G.C.IMPORTANT, scale=0.32}},
+                        {n = G.UIT.T, config={text = ", and ", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                        {n = G.UIT.T, config={text = "8", colour=G.C.IMPORTANT, scale=0.32}},        
+                    }},
+                    {n=G.UIT.R, config={align="cm"}, nodes = {
+                        {n = G.UIT.T, config={text="gives ", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                        {n = G.UIT.T, config={text="+", colour=G.C.CHIPS, scale=0.32}},
+                        {n = G.UIT.O, config={object = DynaText({string = chips_rand, colours = {G.C.CHIPS}, pop_in_rate = 9999999, silent = true, random_element = true, pop_delay = 0.2011, scale = 0.32, min_cycle_time = 0})}},
+                        {n = G.UIT.T, config={text=" chips", colour=G.C.CHIPS, scale=0.32}},
+                        {n = G.UIT.T, config={text=" and ", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                        {n = G.UIT.T, config={text="+", colour=G.C.MULT, scale=0.32}},
+                        {n = G.UIT.O, config={object = DynaText({string = mult_rand, colours = {G.C.MULT}, pop_in_rate = 9999999, silent = true, random_element = true, pop_delay = 0.2011, scale = 0.32, min_cycle_time = 0})}},
+                        {n = G.UIT.T, config={text=" Mult", colour=G.C.MULT, scale=0.32}},
+                        {n = G.UIT.T, config={text=".", colour=G.C.UI.TEXT_DARK, scale=0.32}},
+                    }}
+                }}
             }
         }
 	end,
