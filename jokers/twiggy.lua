70fa4decf3ad686628675d70bb8562af120894d9
DumbAsARoc <timbishop514@gmail.com>
2025-04-28 10:38:24 -0400

    twiggy now works with custom planet cards from other mods

--- jokers/twiggy.lua
+++ jokers/twiggy.lua
@@ -53,11 +53,18 @@ SMODS.Joker {
         
         if context.end_of_round and context.cardarea == G.jokers then
             for i, c in ipairs(G.consumeables.cards) do
-                if c.config.center.set == "Planet" and pseudorandom("twiggy") < G.GAME.probabilities.normal/card.ability.extra.out_of then
-                    card_eval_status_text(c, 'extra', nil, nil, nil, { message = localize("k_level_up_ex") })
-                    update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=c.config.center.config.hand_type,chips = G.GAME.hands[c.config.center.config.hand_type].chips, mult = G.GAME.hands[c.config.center.config.hand_type].mult, level=G.GAME.hands[c.config.center.config.hand_type].level})
-                    level_up_hand(c, c.config.center.config.hand_type)
-                    update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
+                local cen = c.config.center
+                
+                if cen.set == "Planet" and pseudorandom("twiggy") < G.GAME.probabilities.normal/card.ability.extra.out_of then
+                    
+                    if cen.use and type(cen.use) == "function" then
+                        cen:use(c, context.cardarea)
+                    else
+                        card_eval_status_text(c, 'extra', nil, nil, nil, { message = localize("k_level_up_ex") })
+                        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=c.config.center.config.hand_type,chips = G.GAME.hands[c.config.center.config.hand_type].chips, mult = G.GAME.hands[c.config.center.config.hand_type].mult, level=G.GAME.hands[c.config.center.config.hand_type].level})
+                        level_up_hand(c, c.config.center.config.hand_type)
+                        update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
+                    end
                 end
             end
         end
